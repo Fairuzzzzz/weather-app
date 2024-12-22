a76e0a69745ac31893852d3f189a8eb9beb2af66
fairuz <fairuz.afnan@gmail.com>
2024-12-22 21:53:39 +0700

    enhance Days page layout with detailed weather information

--- lib/pages/7days_page.dart
+++ lib/pages/7days_page.dart
@@ -26,16 +26,152 @@ class _DaysPageState extends State<DaysPage> {
           child: Column(
         children: [
           Padding(
-            padding: EdgeInsets.symmetric(vertical: 30, horizontal: 40),
+            padding: EdgeInsets.symmetric(vertical: 30, horizontal: 46),
             child: Stack(
-              alignment: Alignment.center,
               children: [
-                Image.asset(
-                  'assets/images/Cloud.png',
-                  scale: 1.5,
+                Row(
+                  children: [
+                    Image.asset(
+                      'assets/images/Cloud.png',
+                      scale: 1.25,
+                    ),
+                    SizedBox(
+                      width: 24,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Tomorrow',
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 24)),
+                        Row(
+                          children: [
+                            Text(
+                              '20',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 50,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              '/',
+                              style: TextStyle(
+                                  color: Colors.white70,
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              '21',
+                              style: TextStyle(
+                                  color: Colors.white70,
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.w500),
+                            )
+                          ],
+                        ),
+                        Text(
+                          'Cloud',
+                          style: TextStyle(
+                            color: Colors.white60,
+                            fontSize: 20,
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
                 ),
               ],
             ),
+          ),
+          SizedBox(
+            height: 12,
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    Container(
+                      height: 12,
+                      width: 12,
+                      decoration: BoxDecoration(color: Colors.white),
+                    ),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    Text(
+                      '13 km/h',
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      'Wind',
+                      style: TextStyle(fontSize: 14, color: Colors.white70),
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    Container(
+                      height: 12,
+                      width: 12,
+                      decoration: BoxDecoration(color: Colors.white),
+                    ),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    Text(
+                      '24 %',
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      'Humidity',
+                      style: TextStyle(fontSize: 14, color: Colors.white70),
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    Container(
+                      height: 12,
+                      width: 12,
+                      decoration: BoxDecoration(color: Colors.white),
+                    ),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    Text(
+                      '83 %',
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      'Chance of rain',
+                      style: TextStyle(fontSize: 14, color: Colors.white70),
+                    )
+                  ],
+                ),
+              ),
+            ],
           )
         ],
       )),
